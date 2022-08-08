fbe3948d0ceb4b541d803ac337c2f114e1c23429
Gowtham-5 <100989869+Gowtham-5@users.noreply.github.com>
2022-08-08 18:35:34 +0000

    Add files via upload

--- DBS_Transactions/src/main/java/com/DBS/Transaction/Service/TransactionService.java
+++ DBS_Transactions/src/main/java/com/DBS/Transaction/Service/TransactionService.java
@@ -1,5 +1,8 @@
 package com.DBS.Transaction.Service;
 
+import java.util.Optional;
+
+import org.HdrHistogram.DoubleRecordedValuesIterator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +11,7 @@ import com.DBS.Transaction.Model.Customer;
 import com.DBS.Transaction.Model.Transaction;
 import com.DBS.Transaction.Repository.Customer_Repository;
 import com.DBS.Transaction.Repository.TransactionRepository;
+import com.fasterxml.jackson.databind.jsontype.impl.AsDeductionTypeDeserializer;
 
 import antlr.MakeGrammar;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
@@ -29,6 +33,19 @@ public class TransactionService {
 		return transactionRepository.findAll();
 	}
 	
+	public Transaction makeTransaction(Transaction transaction) {
+		Double amount= transaction.getAmount();
+		Integer id=transaction.getCustomers().getCustomerid();
+		Customer customer2= customer_Repository.findById(id).get();
+		Double transferfee= (amount*0.25)/100;
+		Double finalBalance= customer2.getClearbalance()-amount-transferfee;
+    	customer2.setClearbalance(finalBalance);
+    	transaction.setCustomers(customer2);
+    	transaction.setTransferfees(transferfee);
+		return transactionRepository.save(transaction);
 	
+		
+		
+	}
  
 }
